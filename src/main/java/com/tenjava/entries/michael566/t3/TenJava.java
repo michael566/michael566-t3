0a29ad80312cd34db25de76467c4d521a9da1962
Michael <guitar.michael@yahoo.com>
2014-07-12 09:17:53 -0500

    Added onEnable and onDisable method.

--- src/main/java/com/tenjava/entries/michael566/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/michael566/t3/TenJava.java
@@ -2,4 +2,21 @@ package com.tenjava.entries.michael566.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+
+
+    public void onEnable() {
+
+    }
+
+
+
+    public void onDisable() {
+
+    }
+
+
+
+
+}
